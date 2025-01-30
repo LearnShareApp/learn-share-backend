0330e36fa88bdb0cc51d36a3ecc555f9336591a4
Khairullin Ruslan <ruslanrbb8@gmail.com>
2025-01-31 01:37:24 +0300

    local-start

--- docs/docs.go
+++ docs/docs.go
@@ -1549,7 +1549,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "adoe.ru:81",
+	Host:             "localhost:81",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "Learn-Share API",
